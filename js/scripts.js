bccf1b2e97625fd9e8d56e5b7f85365656171084
E-Fitzz <108380326+E-Fitzz@users.noreply.github.com>
2022-07-11 14:47:19 -0400

    added loop in JS with CSS
    Added a loop to iterate pokemonList and added a new pokemon. Added some CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
 //pokedex JavaScript project
 
-var pokemonList = [
+let pokemonList = [
     {
         name: 'Rattata',
-        height: '1\'0\"',
+        height: '1',
         type: 'normal'
     },
    {
         name: 'Charizard',
-        height: '5\'07\"',
+        height: '5.7',
         type: ['fire', 'flying']
     },
     {
         name: 'Geodude',
-        height: '1\'04\"',
+        height: '1.4',
         type: ['rock', 'ground']
+    },
+    {
+        name: 'Diglet',
+        height: '0.8',
+        type: 'ground'
     }
 ];
+
+//loop for pokemonList that iterates their items
+for (let i=0; i<pokemonList.length; i++) {
+    //adds text for any pokemon under a height of 1.0
+    if (pokemonList[i].height <1 && pokemonList[i].height >0){
+      document.write('<p>' + pokemonList[i].name + ' - height: ' + pokemonList[i].height + " (That\'s just a little guy)");
+    //adds text for any pokemon over a height of 4.0
+    }else if (pokemonList[i].height>4){
+      document.write('<p>' + pokemonList[i].name + ' - height: ' + pokemonList[i].height + " (That\'s one chonky boi)");
+    //no extra text for any pokemon between a height of 1.0 and 3.0
+    }else {
+      document.write('<p>' + pokemonList[i].name + ' - height: ' + pokemonList[i].height + " ");
+    }
+  }
