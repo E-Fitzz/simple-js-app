a99d7e26c84514ef6248c0a3d1f3acbc3602726c
E-Fitzz <108380326+E-Fitzz@users.noreply.github.com>
2022-07-17 16:28:43 -0400

    Update scripts.js
    I dont even know

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,56 @@
 //pokedex JavaScript project
-
-let pokemonList = [
-    {
-        name: 'Rattata',
-        height: '1',
-        type: 'normal'
-    },
-   {
-        name: 'Charizard',
-        height: '5.7',
-        type: ['fire', 'flying']
-    },
-    {
-        name: 'Geodude',
-        height: '1.4',
-        type: ['rock', 'ground']
-    },
-    {
-        name: 'Diglet',
-        height: '0.8',
-        type: 'ground'
-    }
+let pokemonRepository = (function(){
+  let pokemonList = [
+      {
+          name: 'Rattata',
+          height: '1',
+          type: 'normal'
+      },
+      {
+          name: 'Charizard',
+          height: '5.7',
+          type: ['fire', 'flying']
+      },
+      {
+          name: 'Geodude',
+          height: '1.4',
+          type: ['rock', 'ground']
+      },
+      {
+          name: 'Diglet',
+          height: '0.8',
+          type: 'ground'
+      }
 ];
 
-//loop for pokemonList that iterates their items
-for (let i=0; i<pokemonList.length; i++) {
-    //adds text for any pokemon under a height of 1.0
-    if (pokemonList[i].height <1 && pokemonList[i].height >0){
-      document.write('<p>' + pokemonList[i].name + ' - height: ' + pokemonList[i].height + " (That\'s just a little guy)");
-    //adds text for any pokemon over a height of 4.0
-    }else if (pokemonList[i].height>4){
-      document.write('<p>' + pokemonList[i].name + ' - height: ' + pokemonList[i].height + " (That\'s one chonky boi)");
-    //no extra text for any pokemon between a height of 1.0 and 3.0
-    }else {
-      document.write('<p>' + pokemonList[i].name + ' - height: ' + pokemonList[i].height + " ");
-    }
+return {
+  add: function(pokemon) {
+     pokemonList.push(pokemon); 
+  },
+  getAll: function() {
+      return pokemonList;
   }
+};
+})();
+
+
+
+//loop for pokemonList that iterates their items
+pokemonList.forEach(function (pokemon){
+  //adds text for any pokemon under a height of 1.0
+  if (pokemon.height <1 && pokemon.height >0) {
+    document.write('<p>' + pokemon.name + ' - height: ' + pokemon.height + " (That\'s just a little guy)");
+    console.log(pokemon.name + ' - height: ' + pokemon.height + " (That\'s just a little guy)");
+
+  //adds text for any pokemon over a height of 4.0
+  }else if (pokemon.height>4){
+    document.write('<p>' + pokemon.name + ' - height: ' + pokemon.height + " (That\'s one chonky boi)");
+  //no extra text for any pokemon between a height of 1.0 and 3.0
+  }else {
+    document.write('<p>' + pokemon.name + ' - height: ' + pokemon.height + " ");
+  }
+});
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Rattata' });
+console.log(pokemonRepository.getAll());
