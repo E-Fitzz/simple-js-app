209e6642368336fac97853d74f78d660295bab80
E-Fitzz <108380326+E-Fitzz@users.noreply.github.com>
2022-07-21 21:59:24 -0400

    update JS
    -Created buttons for pokédex array.
    -Added code to log pokémon info to console on button click.
    -Added CSS to new buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -2,51 +2,70 @@
 
 // ----- IIFE ----- //
 let pokemonRepository = (function(){
-  let pokemonList = [
-      {
-          name: 'Rattata',
-          height: '1',
-          type: 'normal'
-      },
-      {
-          name: 'Charizard',
-          height: '5.7',
-          type: ['fire', ' flying']
-      },
-      {
-          name: 'Geodude',
-          height: '1.4',
-          type: ['rock', ' ground']
-      },
-      {
-          name: 'Diglet',
-          height: '0.8',
-          type: 'ground'
-      }
+    let pokemonList = [
+        {
+            name: 'Rattata',
+            height: '1',
+            type: 'normal'
+        },
+        {
+            name: 'Charizard',
+            height: '5.7',
+            type: ['fire', ' flying']
+        },
+        {
+            name: 'Geodude',
+            height: '1.4',
+            type: ['rock', ' ground']
+        },
+        {
+            name: 'Diglet',
+            height: '0.8',
+            type: 'ground'
+        }
 ];
-
-return {
-  add: function(pokemon) {
-  pokemonList.push(pokemon); 
-  },
-  getAll: function() {
-  return pokemonList;
-  }
+  
+// ----- adds the list to the document
+function add(pokemon) {
+    repository.push(pokemon);
 };
 
+function getAll() {
+    return pokemonList;
+};
+
+// ----- logs pokémon info to the console
+function showDetails(pokemon) {
+    console.log(pokemon);
+};
+
+function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    // ----- creates <li> using JS
+    let pokemonListItem = document.createElement("li");
+    // ----- turns <li> into buttons
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    // ----- adds CSS to the buttons
+    button.classList.add("button-class");
+    // ----- makes buttons out of full array
+    pokemonListItem.appendChild(button);
+    pokemonList.appendChild(pokemonListItem);
+    button.addEventListener('click', function() {
+        showDetails(pokemon);
+    });
+};
+
+return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+};
 })();
+// ----------------- //
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  //adds text for any pokemon under a height of 1.0
-  if (pokemon.height <1 && pokemon.height >0) {
-      document.write('<p>' + pokemon.name + ' - height: ' + pokemon.height + ' - type: ' + pokemon.type + ' (That\'s just a little guy!)');
-  //adds text for any pokemon over a height of 4.0
-  }else if (pokemon.height >4) {
-      document.write('<p>' + pokemon.name + ' - height: ' + pokemon.height + ' - type: ' + pokemon.type + ' (That\'s one chonky boi!)');
-  //no extra text for any pokemon between a height of 1.0 and 3.0
-  }else {
-      document.write('<p>' + pokemon.name + ' - height: ' + pokemon.height + ' - ' + pokemon.type);
-  }
+pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
 });
